31139d63f7c360bfa33765463c567b1f6dd072c3
Daniel Sroka <d.sroka@ela.pl>
2016-10-05 08:56:59 +0200

    feature: add base class for TSP algorithms

---
+++ TSP/Algorithm/TspAlgorithmBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TSP.Algorithm
+{
+    public abstract class TspAlgorithmBase
+    {
+        protected IDictionary<int, int[]> InputRoutes;
+        protected IDictionary<int, int>[] Distances;
+
+
+        public bool IsCalculated { get; }
+
+        public IDictionary<int, int[]> Routes
+        {
+            get
+            {
+                AssertIsCalculated();
+                return InputRoutes;
+            }
+        }
+        
+        public IDictionary<int, int> RoutesLength => Routes.ToDictionary(r => r.Key, r => CalculateRouteLength(r.Value));
+
+        public KeyValuePair<int, int> ShortestRoute
+        {
+            get
+            {
+                AssertIsCalculated();
+                return RoutesLength.OrderBy(r => r.Value).First();
+            }
+        }
+
+        public KeyValuePair<int, int> LongestRoute
+        {
+            get
+            {
+                AssertIsCalculated();
+                return RoutesLength.OrderByDescending(r => r.Value).First();
+            }
+        }
+
+        public double AverageRouteLength
+        {
+            get
+            {
+                AssertIsCalculated();
+                return RoutesLength.Average(r => r.Value);
+            }
+        }
+
+        public abstract IDictionary<int, int[]> CalculateRoutes(
+            IDictionary<int, int>[] distances);
+
+        private void AssertIsCalculated()
+        {
+            if (!IsCalculated)
+                throw new Exception();
+        }
+
+        private int CalculateRouteLength(int[] route)
+        {
+            var routeLength = 0;
+
+            for (var i = 1; i < route.Count(); i++)
+                routeLength += Distances[route[i - 1]][route[i]];
+
+            return routeLength;
+        }
+    }
+}
