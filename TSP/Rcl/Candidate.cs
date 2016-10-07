7015bcff6ce6b557af62ce08dc6eb33b408d501d
Daniel Sroka <d.sroka@ela.pl>
2016-10-07 21:54:13 +0200

    refactor: remove unused overriding methods

--- TSP/Rcl/Candidate.cs
+++ TSP/Rcl/Candidate.cs
@@ -5,26 +5,5 @@
         public int Delta { get; set; }
         public int CityIndex { get; set; }
         public int InsertAt { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(this, obj))
-                return true;
-
-            if (obj == null || GetType() != obj.GetType() || GetHashCode() != obj.GetHashCode())
-                return false;
-
-            return Equals((Candidate)obj);
-        }
-
-        public bool Equals(Candidate obj)
-        {
-            return Delta == obj.Delta && CityIndex == obj.CityIndex && InsertAt == obj.InsertAt;
-        }
-
-        public override int GetHashCode()
-        {
-            return new { Delta, CityIndex, InsertAt }.GetHashCode();
-        }
     }
 }
